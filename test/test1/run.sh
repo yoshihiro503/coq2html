335d9d03b0d5393d08ddf4afac360fc776c9a865
Yoshihiro Imai <y.imai@aist.go.jp>
2025-04-18 10:56:24 +0900

    CI: :bug: setup Rocq for testing

--- test/test1/run.sh
+++ test/test1/run.sh
@@ -8,10 +8,11 @@ RocqNavi=$DIR/../../coq2html
 rm -rf $DIR/html
 mkdir $DIR/html
 
-VFiles="$DIR/Main.v"
+cd $DIR
+VFiles="Main.v"
 coqc $VFiles
 
-GlobFiles="$DIR/Main.glob"
-$RocqNavi -title "test1" -d $DIR/html $VFiles $GlobFiles
+GlobFiles="Main.glob"
+$RocqNavi -title "test1" -d ./html $VFiles $GlobFiles
 
 diff -r $DIR/expected_html $DIR/html
