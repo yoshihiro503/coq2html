0f43cf8489779c6defd8075629b70f23a83839ba
Yoshihiro Imai <y.imai@aist.go.jp>
2025-07-30 12:09:31 +0900

    CI: :construction_worker: fix generating dependey graph

--- tools/generate-infotheo.sh
+++ tools/generate-infotheo.sh
@@ -19,13 +19,13 @@ coqdep -f $CoqProject > depend.d
 cat -n depend.d >&2
 $DIR/ocamldot/ocamldot --style "bgcolor=white; splines=true; nodesep=1; node [fontsize=18, shape=rect, color=\"#dbc3b6\", style=filled];" depend.d > depend.dot
 
-sed -i 's/Lib/infotheo\.lib/' depend.dot
-sed -i 's/Probability/infotheo\.probability/' depend.dot
-sed -i 's/Infomation_theory/infotheo\.infomation_theory/' depend.dot
-sed -i 's/Ecc_classic/infotheo\.ecc_classic /' depend.dot
-sed -i 's/Ecc_modern/infotheo\.ecc_modern /' depend.dot
-sed -i 's/Robust/infotheo\.robust/' depend.dot
-sed -i 's/Toy_examples/infotheo\.toy_examples/' depend.dot
+sed -i 's/lib/infotheo\.lib/' depend.dot
+sed -i 's/probability/infotheo\.probability/' depend.dot
+sed -i 's/infomation_theory/infotheo\.infomation_theory/' depend.dot
+sed -i 's/ecc_classic/infotheo\.ecc_classic/' depend.dot
+sed -i 's/ecc_modern/infotheo\.ecc_modern/' depend.dot
+sed -i 's/robust/infotheo\.robust/' depend.dot
+sed -i 's/toy_examples/infotheo\.toy_examples/' depend.dot
 sed -i 's/\//\./g' depend.dot
 
 $DIR/rocqnavi -title "Infotheo-$REVISION" -d $OUTDIR -base infotheo \
@@ -34,5 +34,6 @@ $DIR/rocqnavi -title "Infotheo-$REVISION" -d $OUTDIR -base infotheo \
   -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.algebra \
   -external https://math-comp.github.io/analysis/htmldoc_1_9_0/ mathcomp.analysis \
   -dependency-graph "depend.dot" \
+  -show-type-infomation-using-coqtop-process \
   $FILES
 
--- tools/generate-mathcomp-analysis.sh
+++ tools/generate-mathcomp-analysis.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 set -eux
-MATHCOMP_ANALYSIS=${MATHCOMP_ANALYSIS:-"./analysis"}
+MATHCOMP_ANALYSIS=${MATHCOMP_ANALYSIS:-"$OPAM_SWITCH_PREFIX/lib/coq/user-contrib/mathcomp/"}
 REVISION=${REVISION:-"no"}
 DIR=$(pwd `dirname .`)
 OUTDIR=${OUTDIR:-$DIR/html/analysis-$REVISION}
-CoqProject=${CoqProject:-_CoqProject}
+ANALYSIS_SRC=${ANALYSIS_SRC:-$OPAM_SWITCH_PREFIX/.opam-switch/sources/coq-mathcomp-analysis.dev}
 INDEX_BLACKLIST_FILE=$DIR/tools/index-blacklist
 
 rm -rf $OUTDIR
 mkdir -p $OUTDIR
 
-cd $MATHCOMP_ANALYSIS
+SRC=$OUTDIR/src
+mkdir $SRC
+
+cp -r $ANALYSIS_SRC/* $SRC/
+
+cd $SRC
 
 ls -l
 
-FILES=$(find analysis classical reals -name "*.v" -or -name "*.glob")
-
-coqdep -f $CoqProject > depend.d
+coqdep -f _CoqProject > depend.d
 cat -n depend.d >&2
 $DIR/ocamldot/ocamldot --style "bgcolor=white; splines=true; nodesep=1; node [fontsize=18, shape=rect, color=\"#dbc3b6\", style=filled];" depend.d > depend.dot
 cat -n depend.dot >&2
 
-sed -i 's/Classical/mathcomp\.classical/' depend.dot
-sed -i 's/Theories/mathcomp\.analysis/' depend.dot
-sed -i 's/Reals_stdlib/mathcomp\.reals_stdlib/' depend.dot
-sed -i 's/Experimental_reals/mathcomp\.experimental_reals/' depend.dot
-sed -i 's/Reals/mathcomp\.reals/' depend.dot
-sed -i 's/Analysis_stdlib/mathcomp\.analysis_stdlib/' depend.dot
-sed -i 's/\//\./g' depend.dot
+sed -i 's|classical/|mathcomp.classical.|' depend.dot
+sed -i 's|theories/|mathcomp.analysis.|' depend.dot
+sed -i 's|reals_stdlib/|mathcomp.reals_stdlib.|' depend.dot
+sed -i 's|experimental_reals/|mathcomp.experimental_reals.|' depend.dot
+sed -i 's|reals/|mathcomp.reals.|' depend.dot
+sed -i 's|analysis_stdlib/|mathcomp.analysis_stdlib.|' depend.dot
+sed -i 's|/|.|g' depend.dot
 
 cat -n depend.dot >&2
 $DIR/tools/generate-hierarchy-graph.sh
+
+cp hierarchy-graph.dot depend.dot $OUTDIR/
+
+
+cd $MATHCOMP_ANALYSIS
+FILES=$(find analysis classical reals -name "*.v" -or -name "*.glob")
 
 $DIR/rocqnavi -title "MathComp-Analysis-$REVISION" -d $OUTDIR \
   -coqlib https://coq.inria.fr/doc/V8.20.1/stdlib/ \
@@ -43,10 +52,7 @@ $DIR/rocqnavi -title "MathComp-Analysis-$REVISION" -d $OUTDIR \
   -Q analysis_stdlib mathcomp.analysis_stdlib \
   -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.ssreflect \
   -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.algebra \
-  -hierarchy-graph "hierarchy-graph.dot" \
-  -dependency-graph "depend.dot" \
+  -hierarchy-graph $OUTDIR/"hierarchy-graph.dot" \
+  -dependency-graph $OUTDIR/"depend.dot" \
   -index-blacklist $INDEX_BLACKLIST_FILE \
-  -show-type-infomation-using-coqtop-process \
   $FILES
-
-cp hierarchy-graph.dot depend.dot $OUTDIR/
