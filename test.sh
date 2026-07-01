6c6536245ab7c9234aef4f470d464da0181c5014
Yoshihiro Imai <y.imai@aist.go.jp>
2026-07-01 18:29:09 +0900

    feat: Testing for deflist

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@ set -e
 test/test1/run.bash
 test/test_type_tooltip/run.bash
 test/hb/run.bash
+test/deflist/run.bash
 
---
+++ test/deflist/run.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eux
+
+DIR=$(cd $(dirname $0) && pwd)
+
+RocqNavi=$DIR/../../rocqnavi
+
+rm -rf $DIR/html
+mkdir $DIR/html
+
+cd $DIR
+VFiles="Main.v"
+rocq compile $VFiles
+
+GlobFiles="Main.glob"
+$RocqNavi -title "test1" -d ./html $VFiles $GlobFiles \
+  -doc-source-url "https://example.com/unexisting/tree/xxxxxxxxxxxxx/"
+
+# Check html files
+if command -v xq >/dev/null 2>&1; then
+    for exp_html in $DIR/expected_html/*.html
+    do
+        base=$(basename $exp_html)
+        act_html=$DIR/html/$base
+        echo "checking $base..."
+        if type xq > /dev/null 2>&1; then
+            diff -u <(xq $exp_html) <(xq $act_html)
+        else
+            diff -uw $exp_html $act_html
+        fi
+    done
+else
+    # Check other resource files and directories
+    diff -ruw -x rocqnavi.css -x rocqnavi.js \
+         $DIR/expected_html $DIR/html
+fi
