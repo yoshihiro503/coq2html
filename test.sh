0419797a5ea54fe2bf912fd4f75ab73a62a2e2be
YOSHIHIRO Imai <y.imai@proof-ninja.co.jp>
2026-08-05 10:06:12 +0900

    chore: :coffin::wastebasket::building_construction::truck::wrench::recycle: DUNE (#180)
    * chore: :coffin::wastebasket::building_construction::truck::wrench::recycle: DUNE

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 set -e
-test/test1/run.bash
-test/test_type_tooltip/run.bash
-test/hb/run.bash
-test/deflist/run.bash
+examples/test1/run.bash
+examples/test_type_tooltip/run.bash
+examples/hb/run.bash
 
 echo 'All test Success!'
