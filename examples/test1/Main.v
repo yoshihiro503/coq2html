b15333fa23b1ec656cbd6f7589bd21cf4194b043
Yoshihiro Imai <y.imai@aist.go.jp>
2026-09-02 14:29:37 +0900

    chore: testings

--- examples/test1/Main.v
+++ examples/test1/Main.v
@@ -3,3 +3,20 @@ Require Import String.
 (** `]a, b[ is a unmatched code block. (rendering will broken same as coqdoc) *)
 (** [[1;2;3] ++ []] is a nested code block example. *)
 Definition x := 123.
+
+Section Export.
+End Export.
+
+Module Mod.
+End Mod.
+
+Module Require.
+End Require.
+
+Import Require.
+
+Module HB.
+End HB.
+From HB Require Import structures.
+
+Locate "_ + _".
